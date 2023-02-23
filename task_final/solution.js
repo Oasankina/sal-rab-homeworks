2e47e566843e3d2b5ea0aaa4d78c375fe6bd0b5d
Oasankina <104391393+Oasankina@users.noreply.github.com>
2023-02-24 02:26:06 +0300

    6.3

--- task_final/solution.js
+++ task_final/solution.js
@@ -49,7 +49,7 @@ function sendRequest(name, phone, address, goods, sum) {
     data.order.sum = sum;
     data.order.client = (name + " " + phone)
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});;
 
     return jsonData;
 }
