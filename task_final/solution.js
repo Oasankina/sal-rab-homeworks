d3a652f2a1ed58ac2cef2e9a8ab37d60931ab11a
Oasankina <104391393+Oasankina@users.noreply.github.com>
2023-02-24 18:10:35 +0300

    7.1

--- task_final/solution.js
+++ task_final/solution.js
@@ -41,7 +41,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i++) {
-        data.goods.push(goods[i]= {title, count});
+        data.goods.push(goods = {goods[i].title, goods[i].count});
     }
 
     data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
